ce7b99858bb6517aae087d12e9b8a120686a0688
Jessiquinterot <152110298+Jessiquinterot@users.noreply.github.com>
2024-11-23 19:14:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function openMenu(language) {
+    let url;
     switch (language) {
         case 'es':
-            window.open('menu_es.html', '_blank');
+            url = 'menu_es.html';
             break;
         case 'pt':
-            window.open('menu_pt.html', '_blank');
+            url = 'menu_pt.html';
             break;
         case 'en':
-            window.open('menu_en.html', '_blank');
+            url = 'menu_en.html';
             break;
         case 'ru':
-            window.open('menu_ru.html', '_blank');
+            url = 'menu_ru.html';
             break;
         default:
             alert('Opción no disponible');
+            return; // Exit the function if the option is not available
     }
+    window.open(url, '_blank');
+    alert('Redirigiendo al menú en ' + language.toUpperCase()); // Confirmation message
 }
